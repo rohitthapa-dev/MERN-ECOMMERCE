d7fcc5a639c9b5980983c73b72eec4c1b1947c7f
PC <pc@Roheets-PCMacBook-Pro.local>
2025-12-23 13:11:58 +0545

    changes in URL

--- frontend/src/app/appUrl.js
+++ frontend/src/app/appUrl.js
@@ -1,2 +1,2 @@
-export const baseUrl = "http://127.0.0.1:5050";
-// export const baseUrl ='https://mernclass.onrender.com';
+//export const baseUrl = "http://127.0.0.1:5050";
+export const baseUrl = "https://mern-ecommerce-yc64.onrender.com";
